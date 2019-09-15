2330a596e6bf136188b5fede287cd9bc4a781849
Jonah Brown <jswish02@gmail.com>
2019-09-15 02:47:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
-def my_each(collection)
-  i=0 
-  while i < collection.length
-  yield(collectin[i])
-  i = i + 1 
-end 
-collection 
 
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
+end
