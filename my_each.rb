cfb36b13c6fd5e5ecf80852598b418500c7423ff
Jonah Brown <jswish02@gmail.com>
2019-09-15 02:40:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,3 @@
-def my_each(array)
-  i = 0 
-  while i < array.length 
-  yield (array[1])
-  i= i+ 1
-end
-end
-my_each(list)
-{|i| puts "so I think #{i} is a piece of "
-
-
-
 
 
 list = ["ab", "cd", "ef"]
