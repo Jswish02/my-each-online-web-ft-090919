323a1b6f64338c38c116b7cabb760e01d7e601db
Jonah Brown <jswish02@gmail.com>
2019-09-15 02:39:02 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0 
+  while i < array.length 
+  yield (array[1])
+  i= i+ 1
 end
+end
+my_each(list)
+{|i| puts "so I think #{i} is a piece of "
